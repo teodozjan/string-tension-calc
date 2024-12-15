660536eaf95164a57db5db5e0e3dec799b3aa895
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2024-12-15 16:01:40 -0300

    Remove wyw, required by former Linaria.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import {defineConfig} from 'vite';
 import {resolve} from 'path';
 import react from '@vitejs/plugin-react';
-import wyw from '@wyw-in-js/vite';
 
 export default defineConfig(_cfg => ({
-	plugins: [
-		react(),
-		wyw(),
-	],
+	plugins: [react()],
 	resolve: {
 		alias: [{
 			find: '@',

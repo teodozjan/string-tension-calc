191476bff4fc56e1e4973f3edac083cc4bb27da8
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2024-12-18 08:40:00 -0300

    Optimize chart rendering.

--- src/component/LineChart.tsx
+++ src/component/LineChart.tsx
@@ -1,22 +1,42 @@
-import {useEffect, useRef, useState} from 'react';
-import {Chart} from 'chart.js/auto';
+import {useEffect, useMemo, useRef, useState} from 'react';
+import {Chart, ChartData} from 'chart.js/auto';
 import styled from '@emotion/styled';
+import {IGuitar} from '@/model/types';
 import {useStore} from '@/model/useStore';
 import {countValidStrings} from '@/model/funcs';
 import * as c from '@/model/consts';
 
 export default function LineChart() {
+	const guitars = useStore(s => s.guitars);
+	const chartData = useChartData(guitars);
 	const canvas = useRef<HTMLCanvasElement | null>(null);
-	useChart(canvas.current);
+	useChart(guitars, chartData, canvas.current);
 
 	return <DivChart>
 		<canvas ref={canvas} />
 	</DivChart>;
 }
 
-function useChart(canvas: HTMLCanvasElement | null) {
-	const unit = useStore(s => s.unit);
-	const guitars = useStore(s => s.guitars);
+function useChartData(guitars: IGuitar[]) {
+	return useMemo(() => {
+		const maxNumStrs = Math.max(...guitars.map(gtr => countValidStrings(gtr)));
+		const labels = c.STRING_NAMES.slice(0, maxNumStrs).reverse();
+		const datasets = guitars.map((gtr, gtrIdx) => ({
+			label: 'Guitar #' + (gtrIdx + 1),
+			data: gtr.strings.map(str => str.tension).slice(0, maxNumStrs).reverse(),
+			tension: .1,
+			pointRadius: 8,
+			pointHoverRadius: 6,
+		}));
+		return {labels, datasets};
+	}, [guitars]);
+}
+
+function useChart(
+	guitars: IGuitar[],
+	chartData: ChartData<'line', number[], string>,
+	canvas: HTMLCanvasElement | null,
+) {
 	const [chart, setChart] = useState<Chart<'line', number[], string> | null>(null);
 
 	useEffect(() => {
@@ -48,20 +68,12 @@ function useChart(canvas: HTMLCanvasElement | null) {
 					chart.destroy();
 					setChart(null);
 				} else {
-					const maxNumStrs = Math.max(...guitars.map(gtr => countValidStrings(gtr)));
-					chart.data.labels = c.STRING_NAMES.slice(0, maxNumStrs).reverse();
-					chart.data.datasets = guitars.map((gtr, gtrIdx) => ({
-						label: 'Guitar #' + (gtrIdx + 1),
-						data: gtr.strings.map(str => str.tension).slice(0, maxNumStrs).reverse(),
-						tension: .1,
-						pointRadius: 8,
-						pointHoverRadius: 6,
-					}));
+					chart.data = chartData;
 					chart.update('none');
 				}
 			}
 		}
-	}, [canvas, unit, guitars, chart]);
+	}, [canvas, guitars, chart, chartData]);
 }
 
 const DivChart = styled.div`

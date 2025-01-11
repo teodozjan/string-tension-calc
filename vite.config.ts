07878e8270c487da2ce1b250a874139e6be5a138
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2025-01-11 09:32:43 -0300

    Version rollback; custom CSS hash.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,28 @@
 import react from '@vitejs/plugin-react';
-import {resolve} from 'path';
+import crypto from 'crypto';
+import path from 'path';
 import {defineConfig} from 'vite';
 
-export default defineConfig(_cfg => ({
+export default defineConfig(cfg => ({
 	plugins: [react()],
 	resolve: {
 		alias: [{
 			find: '@',
-			replacement: resolve(__dirname, 'src'),
+			replacement: path.resolve(__dirname, 'src'),
 		}],
+	},
+	css: {
+		modules: {
+			generateScopedName: (cls, file, _css) => {
+				if (cfg.command === 'serve') {
+					const hash = crypto.createHash('sha1').update(file).digest('hex').substring(0, 5);
+					const fname = path.basename(file).slice(0, -'.module.css'.length);
+					return fname.substring(fname[0] === '+' ? 1 : 0) + '-' + cls + '-' + hash;
+				} else {
+					return '_' + crypto.createHash('sha1').update(file + '__' + cls).digest('hex').substring(0, 10);
+				}
+			},
+		},
 	},
 	base: '/string-tension-calc',
 	server: {
